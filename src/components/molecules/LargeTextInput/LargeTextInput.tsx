e018849ba52172dd2e7d4a8ef583235b3ef1ac4f
aymather <aymather@gmail.com>
2024-01-02 12:47:59 -0600

    created the login & create account screens, did not include functionality

--- src/components/molecules/LargeTextInput/LargeTextInput.tsx
+++ src/components/molecules/LargeTextInput/LargeTextInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, TextInput } from '@atomic'
+import { View, TextInput, Icon } from '@atomic'
 import { useAnimatedState, useTextInput, useTheme } from '@hooks'
 import {
     TouchableWithoutFeedback,
@@ -7,8 +7,10 @@ import {
 } from 'react-native'
 import {
     interpolateColor,
+    useAnimatedProps,
     useAnimatedStyle
 } from 'react-native-reanimated'
+import { IconNames } from '@svg'
 
 
 interface LargeTextInputProps {
@@ -19,6 +21,8 @@ interface LargeTextInputProps {
     onFocus?: () => void
     onBlur?: () => void
     textAlign?: 'left' | 'center'
+    containerProps?: React.ComponentProps<typeof View>
+    icon?: IconNames
 }
 
 const LargeTextInput : React.FC<LargeTextInputProps> = ({
@@ -28,15 +32,16 @@ const LargeTextInput : React.FC<LargeTextInputProps> = ({
     secureTextEntry = false,
     onFocus,
     onBlur,
-    textAlign = 'left'
+    textAlign = 'left',
+    containerProps,
+    icon
 }) => {
 
     const { theme, mode } = useTheme()
 
     const {
         ref,
-        focus,
-        blur
+        focus
     } = useTextInput()
 
     const {
@@ -60,13 +65,31 @@ const LargeTextInput : React.FC<LargeTextInputProps> = ({
         }
     }, [mode])
 
+    const animatedIconStyle = useAnimatedProps(() => {
+        return {
+            fill: interpolateColor(state.value, [0, 1], [theme.colors.t3, theme.colors.t1])
+        }
+    }, [mode])
+
     return (
         <TouchableWithoutFeedback onPress={focus}>
-            <View animated style={[
+            <View animated {...containerProps} style={[
                 styles.container,
                 animatedBorderStyle
             ]}>
-                <View marginHorizontal='xs' marginVertical='s' flexDirection='row' alignItems='center'>
+                {
+                    icon && (
+                        <View marginRight='m'>
+                            <Icon
+                                name={icon}
+                                size='regular'
+                                animated
+                                animatedPathProps={animatedIconStyle}
+                            />
+                        </View>
+                    )
+                }
+                <View marginHorizontal='xs' marginVertical='s' flexDirection='row' alignItems='center' flex={1}>
                     <TextInput
                         ref={ref}
                         defaultValue={defaultValue}
@@ -95,7 +118,9 @@ const LargeTextInput : React.FC<LargeTextInputProps> = ({
 const styles = StyleSheet.create({
     container: {
         width: '100%',
-        borderBottomWidth: 1
+        borderBottomWidth: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
     },
     textInput: {
         fontSize: 22,
--- src/components/pages/Auth/AuthPage/components/index.ts
+++ src/components/pages/Auth/AuthPage/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Loading } from './Loading'
 export { default as Error } from './Error'
+export { default as LogIn } from './LogIn'
+export { default as CreateAccount } from './CreateAccount'
