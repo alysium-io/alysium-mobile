bd92b5e94c9dd7dcd69d2ab6e2d391461a1f25d5
Alec Mather <aymather@gmail.com>
2024-05-14 21:30:49 -0700

    stripping things away for testing

--- App.tsx
+++ App.tsx
@@ -1,19 +1,29 @@
-import { Application, Authentication, Dependencies } from '@arch';
 import React from 'react';
-
-if (__DEV__) {
-	require('./ReactotronConfig');
-}
+import { Text, View } from 'react-native';
 
 // Dependencies: Standard dependencies, app setup, and global state
 // Authentication: Authentication flow
 // Application:  Application navigation/routing (user, artist, host, etc.)
-const App = () => (
-	<Dependencies>
-		<Authentication>
-			<Application />
-		</Authentication>
-	</Dependencies>
-);
+// const App = () => (
+// 	<Dependencies>
+// 		<Authentication>
+// 			<Application />
+// 		</Authentication>
+// 	</Dependencies>
+// );
+const App = () => {
+	return (
+		<View
+			style={{
+				flex: 1,
+				backgroundColor: 'white',
+				justifyContent: 'center',
+				alignItems: 'center'
+			}}
+		>
+			<Text style={{ color: '#000' }}>App</Text>
+		</View>
+	);
+};
 
 export default App;
