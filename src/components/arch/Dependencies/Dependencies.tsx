fcf512803de317d6d292c4f3db1b95a9e6861989
Alec Mather <aymather@gmail.com>
2024-05-15 08:09:29 -0700

    trying with relative imports, removed @flux

--- src/components/arch/Dependencies/Dependencies.tsx
+++ src/components/arch/Dependencies/Dependencies.tsx
@@ -1,6 +1,6 @@
-import { store } from '@flux';
 import React from 'react';
 import { Provider } from 'react-redux';
+import { store } from '../../../redux';
 
 interface DependenciesProps {
 	children?: React.ReactNode;
