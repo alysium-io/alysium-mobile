59608f7a5de98f5124100c46071e8871320a0644
Alec Mather <aymather@gmail.com>
2024-05-15 12:02:49 -0700

    trying without environment validation

--- src/etc/env.ts
+++ src/etc/env.ts
@@ -1,31 +1,37 @@
 import { Env } from '@types';
-import Config from 'react-native-config';
-import * as yup from 'yup';
 
-export const envSchema = yup.object().shape({
-	apiUrl: yup.string().required('API_URL is required'),
-	env: yup
-		.string()
-		.oneOf(['dev', 'prod'], 'ENV must be "dev" or "prod"')
-		.required()
-});
+// export const envSchema = yup.object().shape({
+// 	apiUrl: yup.string().required('API_URL is required'),
+// 	env: yup
+// 		.string()
+// 		.oneOf(['dev', 'prod'], 'ENV must be "dev" or "prod"')
+// 		.required()
+// });
 
 export const validateEnv = (): Env => {
-	try {
-		const validatedEnv = envSchema.validateSync(
-			{
-				apiUrl: Config.API_URL,
-				env: Config.ENV
-			},
-			{ abortEarly: false }
-		);
+	return {
+		apiUrl: 'https://dev-api.alysium.ninja',
+		env: 'dev'
+	};
+	// try {
+	// 	const validatedEnv = envSchema.validateSync(
+	// 		{
+	// 			apiUrl: Config.API_URL,
+	// 			env: Config.ENV
+	// 		},
+	// 		{ abortEarly: false }
+	// 	);
 
-		return validatedEnv as Env;
-	} catch (error) {
-		console.error('Environment variable validation failed:', error);
-		console.log('Config:', Config);
-		throw new Error('Invalid environment configuration');
-	}
+	// 	return validatedEnv as Env;
+	// } catch (error) {
+	// 	console.error('Environment variable validation failed:', error);
+	// 	console.log('Config:', Config);
+	// 	// throw new Error('Invalid environment configuration');
+	// 	return {
+	// 		apiUrl: 'https://dev-api.alysium.ninja',
+	// 		env: 'dev'
+	// 	};
+	// }
 };
 
 const env = validateEnv();
