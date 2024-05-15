bbd015b70431e29f8eb32eacd8e3ae8f66f2b281
Alec Mather <aymather@gmail.com>
2024-05-14 22:20:23 -0700

    trying with just dependencies

--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
-import { Application, Authentication, Dependencies } from '@arch';
+import { Dependencies } from '@arch';
 import React from 'react';
+import { Text, View } from 'react-native';
 
 // Dependencies: Standard dependencies, app setup, and global state
 // Authentication: Authentication flow
 // Application:  Application navigation/routing (user, artist, host, etc.)
+// const App = () => (
+// 	<Dependencies>
+// 		<Authentication>
+// 			<Application />
+// 		</Authentication>
+// 	</Dependencies>
+// );
 const App = () => (
 	<Dependencies>
-		<Authentication>
-			<Application />
-		</Authentication>
+		<View
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+				backgroundColor: 'white'
+			}}
+		>
+			<Text style={{ color: '#000' }}>App</Text>
+		</View>
 	</Dependencies>
 );
 
