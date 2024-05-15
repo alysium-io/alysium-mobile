18bd404ecc189706551ffe4badb25a87f23b666c
Alec Mather <aymather@gmail.com>
2024-05-14 22:38:31 -0700

    stripping down

--- src/components/arch/Dependencies/Dependencies.tsx
+++ src/components/arch/Dependencies/Dependencies.tsx
@@ -1,33 +1,30 @@
-import { GestureHandlerRootView } from '@atomic';
-import { persistor, store } from '@flux';
-import { PortalProvider } from '@gorhom/portal';
-import { ThemeProvider } from '@restyle';
+import { store } from '@flux';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { RootProvider } from 'src/utils/contexts';
 
 interface DependenciesProps {
 	children?: React.ReactNode;
 }
 
+// const Dependencies: React.FC<DependenciesProps> = ({ children }) => {
+// 	return (
+// 		<Provider store={store}>
+// 			<PersistGate loading={null} persistor={persistor}>
+// 				<ThemeProvider>
+// 					<RootProvider>
+// 						<GestureHandlerRootView>
+// 							<SafeAreaProvider>
+// 								<PortalProvider>{children}</PortalProvider>
+// 							</SafeAreaProvider>
+// 						</GestureHandlerRootView>
+// 					</RootProvider>
+// 				</ThemeProvider>
+// 			</PersistGate>
+// 		</Provider>
+// 	);
+// };
 const Dependencies: React.FC<DependenciesProps> = ({ children }) => {
-	return (
-		<Provider store={store}>
-			<PersistGate loading={null} persistor={persistor}>
-				<ThemeProvider>
-					<RootProvider>
-						<GestureHandlerRootView>
-							<SafeAreaProvider>
-								<PortalProvider>{children}</PortalProvider>
-							</SafeAreaProvider>
-						</GestureHandlerRootView>
-					</RootProvider>
-				</ThemeProvider>
-			</PersistGate>
-		</Provider>
-	);
+	return <Provider store={store}>{children}</Provider>;
 };
 
 export default Dependencies;
