be13dccba81649b8b169d02502751c03923c3fd5
Alec Mather <aymather@gmail.com>
2024-08-30 11:52:06 -0700

    reverted after fixing

--- App.tsx
+++ App.tsx
@@ -1,56 +1,30 @@
-import { ActivityIndicator, DisplayEnvironment } from '@atomic';
-import { persistor, store } from '@flux';
-import { ThemeProvider } from '@restyle';
+import { Application, Authentication, Dependencies } from '@arch';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import React from 'react';
-import { Text, View } from 'react-native';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { ValidateEnvProvider } from 'src/utils/contexts';
+import { Scratch } from 'src/components/helpers';
 
 // Dependencies: Standard dependencies, app setup, and global state
 // Authentication: Authentication flow
 // Application:  Application navigation/routing (user, artist, host, etc.)
 const App = () => {
-	// const sandboxMode = false;
+	const sandboxMode = false;
 
-	// if (sandboxMode) {
-	// 	return (
-	// 		<Dependencies>
-	// 			<BottomSheetModalProvider>
-	// 				<Scratch />
-	// 			</BottomSheetModalProvider>
-	// 		</Dependencies>
-	// 	);
-	// }
-
-	// return (
-	// 	<Dependencies>
-	// 		<Authentication>
-	// 			<Application />
-	// 		</Authentication>
-	// 	</Dependencies>
-	// );
+	if (sandboxMode) {
+		return (
+			<Dependencies>
+				<BottomSheetModalProvider>
+					<Scratch />
+				</BottomSheetModalProvider>
+			</Dependencies>
+		);
+	}
 
 	return (
-		<Provider store={store}>
-			<PersistGate loading={<ActivityIndicator />} persistor={persistor}>
-				<ThemeProvider>
-					<ValidateEnvProvider>
-						<View
-							style={{
-								flex: 1,
-								justifyContent: 'center',
-								alignItems: 'center',
-								backgroundColor: '#000'
-							}}
-						>
-							<Text style={{ color: '#fff' }}>Hello World</Text>
-							<DisplayEnvironment />
-						</View>
-					</ValidateEnvProvider>
-				</ThemeProvider>
-			</PersistGate>
-		</Provider>
+		<Dependencies>
+			<Authentication>
+				<Application />
+			</Authentication>
+		</Dependencies>
 	);
 };
 
