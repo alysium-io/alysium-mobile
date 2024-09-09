ca1c0d8b75919af1808c9063bcabd57797b65abd
Alec Mather <aymather@gmail.com>
2024-09-08 17:19:03 -0700

    Fixed the annoying line around the edit icon on the user profile page

--- src/components/molecules/EditableImage/EditIcon.tsx
+++ src/components/molecules/EditableImage/EditIcon.tsx
@@ -1,25 +1,18 @@
 import { Icon, View } from '@atomic';
 import React from 'react';
-import { StyleSheet } from 'react-native';
 
 const EditIcon = () => {
 	return (
-		<View
-			backgroundColor='bg.negative.p'
-			borderColor='bg.p'
-			style={[styles.container]}
-		>
-			<Icon name='pencil' size='s' color='text.negative.p' />
+		<View backgroundColor='bg.p' borderRadius='round' style={{ padding: 3 }}>
+			<View
+				backgroundColor='bg.negative.p'
+				borderRadius='round'
+				style={{ padding: 5 }}
+			>
+				<Icon name='pencil' size='s' color='text.negative.p' />
+			</View>
 		</View>
 	);
 };
-
-const styles = StyleSheet.create({
-	container: {
-		borderRadius: 1000,
-		borderWidth: 2,
-		padding: 5
-	}
-});
 
 export default EditIcon;
