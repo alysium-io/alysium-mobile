58790c2659dd53040f06fe389685b8c6081d1ef2
Alec Mather <aymather@gmail.com>
2024-11-18 16:38:06 -0800

    Edit event location content list item title & subtitle a lot more clear.

--- src/components/pages/EditArtistEvent/components/EditEventLocation.tsx
+++ src/components/pages/EditArtistEvent/components/EditEventLocation.tsx
@@ -10,21 +10,26 @@ interface EditEventLocationProps {
 
 const EditEventLocation: React.FC<EditEventLocationProps> = ({ eventData }) => {
 	const { chooseEventLocationPage } = useNavigation();
-	const { getAddressComponent } = useLocation();
+	const locationApi = useLocation(eventData.event.location);
 
-	const country = getAddressComponent(
-		eventData.event.location?.address_components ?? [],
-		'administrative_area_level_1'
-	);
+	const title = locationApi.build([
+		{ type: 'street_number' },
+		{ type: 'route' }
+	]);
+
+	const subtitle = locationApi.build([
+		{ type: 'locality' },
+		{ type: 'administrative_area_level_1', nameLength: 'short_name' },
+		{ type: 'postal_code' }
+	]);
 
 	return (
 		<View>
 			<ContentListItem
 				onPress={() => chooseEventLocationPage(eventData.event.event_uid)}
 				titleTextProps={{
-					title:
-						eventData.event.location?.formatted_address ?? 'Select a Location',
-					bottomSubtext: country?.long_name ?? ''
+					title: locationApi.hasLocation ? title : 'Select a Location',
+					bottomSubtext: subtitle
 				}}
 				profileImageProps={{
 					borderRadius: 'none',
